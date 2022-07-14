4b49005e3942e50374b05ed894c3c948da77e603
reHAN-ERP <rehanlyfsolutionslk56@gmail.com>
2022-07-14 15:45:31 +0530

    Update PassportAuthController.php

--- app/Http/Controllers/Api/PassportAuthController.php
+++ app/Http/Controllers/Api/PassportAuthController.php
@@ -11,26 +11,26 @@ use DB;
 class PassportAuthController extends Controller
 {
 
- public function MobileLogin(Request $request){
+   public function MobileLogin(Request $request){
 
-     $request->validate([
-         'email' => 'required',
-         'password' => 'required',
-     ]);
-     $user_login = DB::table('users')->where('email', '=', $request->get('email'))->get();
+       $request->validate([
+           'email' => 'required',
+           'password' => 'required',
+       ]);
+       $user_login = DB::table('users')->where('email', '=', $request->get('email'))->get();
 
 
-     if(count($user_login) !=0){
-         foreach($user_login as $user){
-           if(Hash::check($request->get('password'), $user->password)){
-            return response()->json(['data' => $user_login], 200);
-        }else{
-            return response()->json(['data' => "User name or password is incorrect!"], 200);
+       if(count($user_login) !=0){
+           foreach($user_login as $user){
+             if(Hash::check($request->get('password'), $user->password)){
+                return response()->json(['data' => array('info'=>$user_login,'error'=>null)], 200);
+            }else{
+                return response()->json(['data' => array('info'=>[],'error'=>'User name or password is incorrect!') ], 200);   
+            }
         }
+    }else{
+        return response()->json(['data' => array('info'=>[],'error'=>'Please check your credentials!') ], 401);   
     }
-}else{
-    return response()->json(['data' => "Please check your credentials!"], 401);
-}
 }
 
 
@@ -56,4 +56,5 @@ public function MobileGetInventory(Request $request){
 
 
 }
+
 
