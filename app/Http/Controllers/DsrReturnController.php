6a13219b15a2c8a09b8279c5b67803b7ae340f66
reHAN-ERP <rehanlyfsolutionslk56@gmail.com>
2022-07-25 15:01:21 +0530

    update stock bug 1

--- app/Http/Controllers/DsrReturnController.php
+++ app/Http/Controllers/DsrReturnController.php
@@ -15,24 +15,21 @@ class DsrReturnController extends Controller
     public function DsrReturn(Request $request){
 
     // need to change the query according to mobile side
-     $results = DB::table('dsr_returns')
-     ->join('items', 'dsr_returns.item_id', 'items.id')
-     ->join('users', 'dsr_returns.dsr_id', 'users.id')
-     ->select('items.id as item_id','items.name','items.created_at','dsr_returns.id as return_id','dsr_returns.qty','users.name as user_name')
-     ->where('dsr_returns.status','=',0)
-     ->orderBy('items.name','asc')
-     ->paginate(5);
+       $results = DB::table('dsr_returns')
+       ->join('items', 'dsr_returns.item_id', 'items.id')
+       ->join('users', 'dsr_returns.dsr_id', 'users.id')
+       ->select('items.id as item_id','items.name','items.created_at','dsr_returns.id as return_id','dsr_returns.qty','users.name as user_name')
+       ->where('dsr_returns.status','=',0)
+       ->orderBy('items.name','asc')
+       ->paginate(5);
 
-     return view('admin.item.dsr_receive', ['returnData'=>$results]);
- }
-
-
+       return view('admin.item.dsr_receive', ['returnData'=>$results]);
+   }
 
 
- public function UpdateReturnItems(Request $request){
 
-     // update stock (+)
-    // $update_item_qty = DB::table('items')->where('id','=',$request->item_id)->increment('qty', $request->qty);
+
+   public function UpdateReturnItems(Request $request){
 
     $return_data = DB::table('dsr_retun_no')->join('dsr_returns', 'dsr_returns.id', 'dsr_retun_no.dsr_return_id')
     ->select('dsr_stock_id','qty')->where('dsr_returns.status','=',0)->where('dsr_returns.id','=',$request->get('id'))->get();
@@ -50,20 +47,21 @@ class DsrReturnController extends Controller
                 $deduct_stock = $sid->id;
                 break;
             }
-
         }
     }
-
-
-    // update dsr stock (-)
-    $update_dsr_qty = DB::table('dsr_stock_items')->where('id','=',$deduct_stock)->decrement('qty', $ids->qty);
 
     
         // $update_dsr_qty = DB::table('dsr_stock_items')->where('item_id','=',$request->item_id)->where('dsr_stock_id','=',$ids->dsr_stock_id)->decrement('qty', $ids->qty);
 
 
+    // update dsr stock (-)
+    $update_dsr_qty = DB::table('dsr_stock_items')->where('id','=',$deduct_stock)->decrement('qty', $ids->qty);
+
+       // update stock (+)
+    $update_item_qty = DB::table('items')->where('id','=',$request->item_id)->increment('qty', $request->qty);
+
     // update return table status as 1
-    // $update_return_status = DB::table('dsr_returns')->where('id','=',$request->id)->update(['status'=>1]);
+    $update_return_status = DB::table('dsr_returns')->where('id','=',$request->id)->update(['status'=>1]);
 
     return response("ok");
 
