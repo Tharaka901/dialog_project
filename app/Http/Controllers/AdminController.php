e15a63d22a2eaeb82a4d7d72321409cc5073b5d9
reHAN-ERP <rehanlyfsolutionslk56@gmail.com>
2022-07-26 09:43:31 +0530

    send item qty not 0

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -16,55 +16,32 @@ class AdminController extends Controller
 
   public function main(Request $request){
     return view('admin.login');
- }
+  }
 
- public function Login(Request $request){
+  public function Login(Request $request){
     $request->validate([
-       'email' => 'required',
-       'password' => 'required',
-    ]);
+     'email' => 'required',
+     'password' => 'required',
+   ]);
 
     $admin_login = Admin::all()->where('email', '=', $request->get('email'));
     if(count($admin_login) != 0){
       if(Hash::check($request->get('password'), $admin_login[0]->password)){
-         $request->session()->put('user_id', $admin_login[0]->id);
-         $request->session()->put('user_name', $admin_login[0]->name);
-         return redirect()->route('dashboard');
-      }else{
-         Alert::warning('Oops..', 'User name or password is incorrect!');
-         return redirect()->back();
-      }
+       $request->session()->put('user_id', $admin_login[0]->id);
+       $request->session()->put('user_name', $admin_login[0]->name);
+       return redirect()->route('dashboard');
+     }else{
+       Alert::warning('Oops..', 'User name or password is incorrect!');
+       return redirect()->back();
+     }
    }else{
      Alert::warning('Oops..', 'Please check your credentials!');
      return redirect()->back();
-  }
-}
-
-// public function MobileLogin(Request $request){
-//     $request->validate([
-//        'email' => 'required',
-//        'password' => 'required',
-//     ]);
-
-//     $admin_login = Admin::all()->where('email', '=', $request->get('email'));
-//     if(count($admin_login) != 0){
-//       if(Hash::check($request->get('password'), $admin_login[0]->password)){
-//          $request->session()->put('user_id', $admin_login[0]->id);
-//          $request->session()->put('user_name', $admin_login[0]->name);
-//          return redirect()->route('dashboard');
-//       }else{
-//          Alert::warning('Oops..', 'User name or password is incorrect!');
-//          return redirect()->back();
-//       }
-//    }else{
-//      Alert::warning('Oops..', 'Please check your credentials!');
-//      return redirect()->back();
-//   }
-// }
+   }
+ }
 
 
-
-public function Logout(Request $request){
+ public function Logout(Request $request){
   $request->session()->flush();
   return redirect()->route('/');
 }
--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Http\Response;
+use DB;
 class DashboardController extends Controller
 {
     public function Dashboard(){
-        return view('admin.index');
-    }
+
+         //set dashboard data
+     $dashboard_data = DB::table('pending_sum')
+     ->join('users', 'pending_sum.dsr_id', 'users.id')
+     ->select('pending_sum.id','pending_sum.dsr_id','users.name','date','inhand_sum','sales_sum','credit_sum','credit_collection_sum','banking_sum','direct_banking_sum','retialer_sum')
+     ->where('date', '=', $request->get('date'))
+     ->where('pending_sum.dsr_id', '=', $request->get('dsr_id'))
+     ->get();
+
+
+     return view('admin.index','itemData'=>$dashboard_data);
+ }
 }
