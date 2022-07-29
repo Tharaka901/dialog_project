a9c7dc6c9a01db4606a174becec0df3fd01e6fc0
reHAN-ERP <rehanlyfsolutionslk56@gmail.com>
2022-07-29 09:39:38 +0530

    2022-07-29-3

--- app/Http/Controllers/DsrController.php
+++ app/Http/Controllers/DsrController.php
@@ -27,7 +27,7 @@ class DsrController extends Controller
         ->join('users', 'pending_sum.dsr_id', 'users.id')
         ->select('pending_sum.id','pending_sum.dsr_id','users.name','date','inhand_sum','sales_sum','credit_sum','credit_collection_sum','banking_sum','direct_banking_sum','retialer_sum')
         ->where('date', '=', $todayDate)
-        ->where('pending_sum.status', '=', 0)->paginate(5);
+        ->where('pending_sum.status', '=', 1)->paginate(5);
 
         return view('admin.dsr.pending_dsr',["dsrData"=>$pdsr]);
     }
